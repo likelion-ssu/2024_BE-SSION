a8e74c8e5e358c0b15c8d4fbbe991458dfacaedb
shinminkyoung1 <dottiee99@naver.com>
2024-06-16 19:55:15 +0900

    add file

---
+++ 4주차/신민경/bookapp/views.py
@@ -0,0 +1,174 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from bookapp.serializers import BookSerializer
+from bookapp.models import Book
+from rest_framework import status
+from reviewapp.models import Review
+from reviewapp.serializers import BookReviewSerializer
+
+
+# Create your views here.
+class BookListView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class BookDetailsView(APIView):
+    def get(self, request, id):
+        try :
+            book = Book.objects.get(id=id)
+        except Book.DoesNotExist:
+            return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        try :
+            book = Book.objects.get(id=id)
+        except Book.DoesNotExist:
+            return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)        
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, id):
+        try :
+            book = Book.objects.get(id=id)
+        except Book.DoesNotExist:
+            return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# GET /api/books/{id}/reviews    
+class BookReviewListView(APIView):
+    def get_book(self, id):
+        try:
+            return Book.objects.get(id=id)
+        except Book.DoesNotExist:
+            return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id):
+        book = self.get_book(id)
+        reviews = book.reviews.all()
+        serializer = BookReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, id):
+        serializer = BookReviewSerializer(data=request.data)
+        book = self.get_book(id)
+        if serializer.is_valid():
+            serializer.save(
+                book=book
+            )
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        
+class BookReviewDetailView(APIView):
+    def get(self, request, id, review_id):
+        # {id}번 도서의 {review_id}번 리뷰 조회
+        try:
+            book = Book.objects.get(id=id)
+            review = Review.objects.get(id=review_id, book=book)
+        except Book.DoesNotExist:
+            return Response({'message': '도서의 리뷰를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = BookReviewSerializer(review)
+        return Response(serializer.data)
+
+    def put(self, request, id, review_id):
+        # {id}번 도서의 {review_id}번 리뷰 수정
+        try:
+            book = Book.objects.get(id=id)
+            review = Review.objects.get(id=review_id, book=book)
+        except (Book.DoesNotExist, Review.DoesNotExist):
+            return Response({'message': '도서의 리뷰를 찾지 못했습니다.'}, statue=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = BookReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id, review_id):
+        #{id}번 도서의 {review_id}번 리뷰 삭제
+        try: 
+            book = Book.objects.get(id=id)
+            review = Review.objects.get(id=review_id, book=book)
+        except (Book.DoesNotExist, Review.DoesNotExist):
+            return Response({'message': '도서의 리뷰를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        review.delete()
+        return Response(status=status.HTTP_404_NOT_FOUND) # 이 상태 코드가 맞나..
+
+
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.views import api_view
+# from bookapp.serializers import BookSerializer
+# from bookapp.models import Book
+# from rest_framework import status
+
+
+# # Create your views here.
+# @api_view(['GET', 'POST'])
+# def book_list(request):
+#     if request.method == 'GET':
+#         books = Book.objects.all()
+#         serializer = BookSerializer(books, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def book_details(request, id):
+#     if request.method == 'GET':
+#         try :
+#             book = Book.objects.get(id=id)
+#         except Book.DoesNotExist:
+#             return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_404_NOT_FOUND)
+#         serializer = BookSerializer(book)
+#         return Response(serializer.data)
+    
+#     elif request.method == 'PUT':
+#         try :
+#             book = Book.objects.get(id=id)
+#         except Book.DoesNotExist:
+#             return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)        
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+#     elif request.method == 'DELETE' : 
+#         try :
+#             book = Book.objects.get(id=id)
+#         except Book.DoesNotExist:
+#             return Response({'message': '도서를 찾지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
