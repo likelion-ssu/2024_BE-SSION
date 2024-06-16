abb75a8a9b031dc106da2fac211b0fc9104692d0
Kseonjeong <sj030205@naver.com>
2024-06-16 20:20:17 +0900

    feat: assignment 4

---
+++ 4주차/김선정/bookapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from bookapp import views
+
+urlpatterns=[
+  path('',views.BookListView.as_view()),
+  path('<int:id>/',views.BookDetailsView.as_view()),
+  path('<int:id>/reviews/',views.BookReviewListView.as_view()),
+  path('<int:id>/reviews/<int:reviews_id>/',views.BookReviewDetailView.as_view()),
+  
+]
